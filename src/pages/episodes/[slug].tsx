80a1bd23ebb238933ebb233396b983b5ab491755
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-04-26 00:02:46 -0300

    feat: title

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,10 +2,12 @@ import { format, parseISO } from "date-fns";
 import { enUS } from "date-fns/locale";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 import { GetStaticPaths, GetStaticProps } from "next";
 
 import { api } from "../../services/api";
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+import { usePlayer } from "../../contexts/PlayerContext";
 
 import styles from "./episode.module.scss";
 
@@ -26,8 +28,14 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
+
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
@@ -40,7 +48,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Play episode" />
         </button>
       </div>
